1cc3affeab2cb2b3f5ddbddc6faba52ad64bffbc
chai_chate <chai_chate@hotmail.com>
2020-04-24 16:25:49 +0700

    add

--- work07_4_621202064109.c
+++ work07_4_621202064109.c
@@ -2,68 +2,62 @@
 #include <stdlib.h>
 #include <curses.h>
 
-int main (){
+int main()
+{
+     float price = 0, net = 0, total = 0, discount = 0, total_discount = 0 , sum = 0;
+     char prodCode[10], prodName[20], ans = 'y';
+     int amount = 0;
 
-     char prodCode[10],prodName[20] , ans = 'y' ;
-     int amount= 0 , total_amount = 0;
-     float price=0  , net= 0  , total  = 0 , discount = 0 , total_discount = 0; 
+#ifdef _WIN32
+     system("cls");
+#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+     system("clear");
+#endif
 
-     #ifdef _WIN32
-          system("cls");
-     #elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
-          system("clear");
-     #endif
-
-     
-     
      do
      {
           printf("Input Product Code : ");
-          scanf("%s" ,  prodCode);
+          scanf("%s", prodCode);
 
           printf("Input Product Name : ");
-          scanf("%s" ,  prodName);
+          scanf("%s", prodName);
 
           printf("Input Amount : ");
-          scanf("%d" ,  &amount );
+          scanf("%d", &amount);
 
           printf("Input Price/Unit : ");
-          scanf("%f" ,  &price );
+          scanf("%f", &price);
 
-          printf("More (y/n) : "); 
-          ans = getch();
+          if (amount < 150)
+          {
+               discount = 0;
+          }
+          else if (amount >= 150 && amount <= 500)
+          {
+               discount = 3;
+          }
+          else if (amount > 500 && amount <= 1200)
+          {
+               discount = 5;
+          }
+          else if (amount > 1200)
+          {
+               discount = 10;
+          }
 
-          printf("ans = %c : ", ans );
+
+          total = amount * price;
+          total_discount = (total * discount/100);
+          net = total - total_discount;
 
 
-          total += amount * price ;
-          total_amount += amount ;
+          printf("Discount = %.2f\n" , total_discount);
+          printf("Total = %.2f\n" , net);
 
-         
+          printf("More (y/n) : ");
+          ans = getche();
 
-     } while (ans == 'y' );
-     
-
-     if(total_amount < 150)
-     {
-          discount = 0;
-     }
-     else if (total_amount >= 150 && total_amount <= 500)
-     {
-          discount = 3;
-     }
-     else if(total_amount > 500 && total_amount <= 1200)
-     {
-           discount = 5;
-     }
-     else if (total_amount > 1200)
-     {
-          discount = 10 ;
-     }
-     
-     total_discount = (discount * total / 100) ;
-     net = total - total_discount ;
-
+     } while (ans == 'y');
 
      return 0;
 }
